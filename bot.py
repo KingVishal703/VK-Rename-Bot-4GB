f50a7a4b1832111edbdfb8434488275fbe34210c
KingVishal703 <vishal21wankhede@gmail.com>
2026-02-19 18:46:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,29 +3,52 @@ from plugins.cb_data import app as Client2
 from config import *
 import pyromod
 import pyrogram.utils
+import threading
+from flask import Flask
 
 pyrogram.utils.MIN_CHAT_ID = -999999999999
 pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
+bot = Client(
+    "Renamer",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    plugins=dict(root='plugins')
+)
 
+# -------------------- FLASK SERVER FOR KOYEB --------------------
 
-bot = Client("Renamer", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root='plugins'))
+web_app = Flask(__name__)
 
+@web_app.route("/")
+def home():
+    return "Bot is running!"
 
+def run_web():
+    web_app.run(host="0.0.0.0", port=8000)
 
+# ---------------------------------------------------------------
 
 if STRING_SESSION:
-    apps = [Client2,bot]
+    apps = [Client2, bot]
+
     for app in apps:
         app.start()
+
+    # Start Flask in background thread
+    threading.Thread(target=run_web).start()
+
     idle()
+
     for app in apps:
         app.stop()
-    
+
 else:
+    # Start Flask in background thread
+    threading.Thread(target=run_web).start()
+
     bot.run()
-
-
 
 
 # Jishu Developer 
