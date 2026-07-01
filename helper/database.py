ef17e9dab4086ba04d0a0aeb53c3dc3d55112fb6
KingVishal703 <vishal21wankhede@gmail.com>
2026-07-01 12:55:07 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -41,7 +41,11 @@ def insert(chat_id):
     user_id = int(chat_id)
     user_det = {"_id": user_id, "file_id": None, "caption": None, "daily": 0, "date": 0,
                 "uploadlimit": 5368709120, "used_limit": 0, "usertype": "Free", "prexdate": None,
-                "metadata": False, "metadata_code": "By @Madflix_Bots"}
+                "metadata": False, "metadata_code": "By @Madflix_Bots",
+                "intro": False, "intro_file": None, "outro": False, "outro_file": None, "watermark": False,
+                "watermark_file": None, "watermark_position": "top-right", }
+    
+
     try:
         dbcol.insert_one(user_det)
     except:
@@ -78,6 +82,32 @@ def addcaption(chat_id, caption):
 def delcaption(chat_id):
     dbcol.update_one({"_id": chat_id}, {"$set": {"caption": None}})
 
+
+# ================= VIDEO EDIT SETTINGS ================= #
+
+def set_intro(chat_id, status):
+    dbcol.update_one({"_id": chat_id}, {"$set": {"intro": status}})
+
+def set_intro_file(chat_id, file_id):
+    dbcol.update_one({"_id": chat_id}, {"$set": {"intro_file": file_id}})
+
+
+def set_outro(chat_id, status):
+    dbcol.update_one({"_id": chat_id}, {"$set": {"outro": status}})
+
+def set_outro_file(chat_id, file_id):
+    dbcol.update_one({"_id": chat_id}, {"$set": {"outro_file": file_id}})
+
+
+def set_watermark(chat_id, status):
+    dbcol.update_one({"_id": chat_id}, {"$set": {"watermark": status}})
+
+def set_watermark_file(chat_id, file_id):
+    dbcol.update_one({"_id": chat_id}, {"$set": {"watermark_file": file_id}})
+
+
+def set_watermark_position(chat_id, position):
+    dbcol.update_one({"_id": chat_id}, {"$set": {"watermark_position": position}})
 
 
 def dateupdate(chat_id, date):
@@ -121,10 +151,61 @@ def find(chat_id):
             metadata_code = i["metadata_code"]
         except:
             metadata_code = None
+
+        try:
+            intro = i["intro"]
+        except:
+            intro = False
+
+        try:
+            intro_file = i["intro_file"]
+        except:
+            intro_file = None
+
+        try:
+            outro = i["outro"]
+        except:
+            outro = False
+
+        try:
+            outro_file = i["outro_file"]
+        except:
+            outro_file = None
+
+        try:
+            watermark = i["watermark"]
+        except:
+            watermark = False
+
+        try:
+            watermark_file = i["watermark_file"]
+        except:
+            watermark_file = None
+
+        try:
+            watermark_position = i["watermark_position"]
+        except:
+            watermark_position = "top-right"
             
 
 
-        return [file, caption, metadata, metadata_code]
+        return [
+            file,
+            caption,
+            metadata,
+            metadata_code,
+
+           intro,
+           intro_file,
+
+           outro,
+           outro_file,
+
+           watermark,
+           watermark_file,
+
+           watermark_position
+      ]
 
 def getid():
     values = []
